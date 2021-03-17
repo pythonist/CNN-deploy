3d1f524e7233999b1b19dd4302c32d2ea9ea6e3a
pythonist <44141931+pythonist@users.noreply.github.com>
2021-03-17 16:54:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,7 @@ import os
 import glob
 import re
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
 
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
